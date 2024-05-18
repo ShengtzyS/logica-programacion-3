75dc1d8e475f900759d505bb1d54df3a05423489
ShengtzyS <shengtzy.subaique@genstudents.org>
2024-05-17 23:08:27 -0400

    Tarea final lógica-programación-3

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Función para calcular factorial
 function factorial(n) {
     if (n < 0) {
-        alert("Error: Debe ingresar un número mayor a 0");
+        alert("Error: Debe ingresar un número mayor a 0.");
     } else if (isNaN(n)) {
-        alert("Error: Debe ingresar un número");
+        alert("Error: Debe ingresar un número.");
     } else if (n === 0 || n ===1){
         return 1;
     } else {
@@ -11,7 +12,17 @@ function factorial(n) {
                 
          }
     
-    let n = Number(prompt("Ingrese un número: "));
-    console.log(factorial(n));
-    
+// Ciclo while para que vuelva a solicitar dato si se escriben palabras
+    let n;
+    while (true) {
+        let dato = prompt("Ingrese un número: ");
+        if (dato === null) break;
+        n = Number(dato);
+        if (!isNaN(n)){
+            console.log(factorial(n));
+            break;
+        } else {
+            alert("Error: Debe ingresar un número.")
+        }
+    }
 
